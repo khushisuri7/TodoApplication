316d525d5fe25c64bfcedc4a78c25e5b53afa06e
khushi <khushisuri9252gmail.com>
2022-08-02 01:24:23 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Alert ,TouchableWithoutFeedback, Keyboard} from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
+import Sandbox from './components/sandbox';
 
 export default function App() {
+  const [todos, setTodos]=useState([
+    {text: 'buy coffee', key:'1'},
+    {text: 'create an app', key:'2'},
+    {text: 'play on the switch', key:'3'}
+
+  ]);
+  const pressHandler=(key)=>{
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo => todo.key!=key);
+    });
+  }
+
+  const submitHandler = (text) => {
+    if(text.length > 3){
+      setText('');
+      setTodos(prevTodos => {
+        return [
+          { text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    } else {
+      Alert.alert('OOPS', 'Todo must be over 3 characters long', [
+        {text: 'Understood', onPress: () => console.log('alert closed') }
+      ]);
+    }
+  };
+
   return (
+    // <Sandbox/>
+    <TouchableWithoutFeedback onPress={()=>{
+      Keyboard.dismiss();
+    }}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler}/>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item})=>(
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -14,7 +62,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  content:{
+    flex:1,
+    padding:40,
+  },
+  list:{
+    flex:1,
+    marginTop:20,
+  }
 });
